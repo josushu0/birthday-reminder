38de1094c4423343b5c771f9eb069e352cd21a61
Josué Martell <josue.martell17@gmail.com>
2023-02-03 11:21:28 -0600

    feat(navigation): created a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+	return (
+		<div className="container">
+			<div className="navbar">
+				<h1>Retro Off</h1>
+				<button className="button">Nuevo</button>	
+			</div>	
+		</div>
+	)
+}
+
+export default Navbar
