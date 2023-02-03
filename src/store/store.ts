564fc8beefe75d35fe9cbb609c479ec906bb0a53
Josué Martell <josue.martell17@gmail.com>
2023-02-03 11:23:07 -0600

    feat(redux): added reducer to store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import birthdaysReducer from './slices/bithdays/birthdaysSlice'
 
 export const store = configureStore({
-	reducer: {}
+	reducer: {
+		birthdays: birthdaysReducer
+	}
 })
 
 export type RootState = ReturnType<typeof store.getState>
