846aca003ae3dbf84f6aae65aa2210bee16b6537
Josué Martell <josue.martell17@gmail.com>
2023-02-06 14:44:04 -0600

    feat(home): show countdown only when there are less than a week to birthday

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,29 @@
 import { birthdayInfo } from "../types"
 import dayjs from "dayjs"
 import "dayjs/locale/es"
+import Countdown from "./Countdown"
+import { useState } from "react"
 
 function Card({birthdayInfo}: {birthdayInfo: birthdayInfo}) {
+	const [active, setActive] = useState(false)
 	const { name, cake, date} = birthdayInfo
-	const dateFormat = dayjs(date).locale("es").format('D MMMM')
+	const today = dayjs()
+	const dateObject = dayjs(date)
+	const dateFormat = dateObject.locale("es").format('D MMMM')
+	if (dateObject.diff(today, 'day') <= 7) {
+		setActive(true)
+	}
 
 		return (
 		<div className="card">
 			<img src={birthdayInfo.photo} className="card__image" />
 			<div className="card__info">
+					<>
+						<Countdown date={dateObject} />
+						<p>Pastel favorito: {cake}</p>
+					</>
 				<p>Nombre: {name}</p>
 				<p>Cumpleaños: {dateFormat}</p>	
-				<p>Pastel favorito: {cake}</p>
 			</div>	
 		</div>
 	)
