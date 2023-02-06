e94166085e868538df83bc931db1dfc738b1e2a1
Josué Martell <josue.martell17@gmail.com>
2023-02-06 00:22:13 -0600

    feat(home): display localized birthday

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,17 @@
 import { birthdayInfo } from "../types"
+import dayjs from "dayjs"
+import "dayjs/locale/es"
 
 function Card({birthdayInfo}: {birthdayInfo: birthdayInfo}) {
 	const { name, cake, date} = birthdayInfo
+	const dateFormat = dayjs(date).locale("es").format('D MMMM')
+
 		return (
 		<div className="card">
 			<img src={birthdayInfo.photo} className="card__image" />
 			<div className="card__info">
 				<p>Nombre: {name}</p>
-				<p>Cumpleaños: {date}</p>	
+				<p>Cumpleaños: {dateFormat}</p>	
 				<p>Pastel favorito: {cake}</p>
 			</div>	
 		</div>
