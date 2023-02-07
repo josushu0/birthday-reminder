447c6589fd3c1322751c8c6ea5790e2f4caf1efa
Josué Martell <josue.martell17@gmail.com>
2023-02-07 11:07:08 -0600

    feat(new): created a form to add a birthday

---
+++ src/components/BirthdayForm.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react"
+import dayjs from "dayjs"
+import { pb } from "../pocketbase/pocketbase"
+import { NavLink } from "react-router-dom"
+
+interface form {
+	name: string,
+	cake: string,
+	date: string,
+	photo: File | string
+}
+
+function BirthdayForm() {
+	const [formState, setFormState] = useState<form>({
+		name: '',
+		cake: '',
+		date: '',
+		photo: ''
+	})
+
+	const formData = new FormData()
+
+	async function handleSubmit(e: React.FormEvent) {
+		e.preventDefault()
+		formData.append('name', formState.name)
+		formData.append('cake', formState.cake)
+		formData.append('date', dayjs(formState.date).toISOString())
+		formData.append('photo', formState.photo)
+		await pb.collection('birthdays').create(formData)
+	}
+
+	function onInputChange({target}: {target: HTMLInputElement}) {
+		setFormState({
+			...formState,
+			[target.name]: target.value
+		})
+	}
+
+	function onFileSubmit({target}: {target: HTMLInputElement}) {
+		formData.delete('photo')
+		for(let file of target.files!) {
+			setFormState({
+				...formState,
+				photo: file
+			})
+		}
+	}
+
+	return (
+		<div className="container">
+			<form className="birthdayForm" onSubmit={handleSubmit}>
+				<h1>Añadir cumpleaños</h1>
+				<label htmlFor="photo">Fotografía</label>
+				<input type="file" id="photo" name="photo" required onChange={onFileSubmit}/>
+				<label htmlFor="name">Nombre</label>
+				<input type="text" id="name" name="name" required onChange={onInputChange}/>
+				<label htmlFor="cake">Pastel favorito</label>
+				<input type="text" id="cake" name="cake" required onChange={onInputChange}/>
+				<label htmlFor="date">Fecha de nacimiento</label>
+				<input type="date" id="date" name="date" required onChange={onInputChange}/>
+				<div className="buttons">
+					<NavLink className="button button_cancel" to="/">Cancelar</NavLink>
+					<button className="button" type="submit">Añadir</button>
+				</div>
+			</form>
+		</div>
+	)
+}
+
+export default BirthdayForm
